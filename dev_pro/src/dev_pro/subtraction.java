d9d9fe3e88839f0056332ddad882db0b62d50a93
aarth <aarth@Aarthi>
2023-08-02 15:13:44 +0530

    A JAVA subtraction program.

---
+++ dev_pro/src/dev_pro/subtraction.java
@@ -0,0 +1,21 @@
+package dev_pro;
+
+public class subtraction {
+
+	public static void main(String[] args) {
+
+		int x=50;
+		int y=10;
+
+		int subtract;
+
+		subtract = x - y;
+
+		System.out.println("The substraction of two Numbers: " + subtract);
+
+	}
+
+}
+
+
+
